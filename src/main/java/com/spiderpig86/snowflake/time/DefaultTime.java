558cc81949767d247ee8c4f00325f5e3e6a49e4f
ambition85 <ambition9385@gmail.com>
2023-05-07 01:24:26 -0700

    :sparkles: feat(DefaultTime.java): Add method to get default version

--- src/main/java/com/spiderpig86/snowflake/time/DefaultTime.java
+++ src/main/java/com/spiderpig86/snowflake/time/DefaultTime.java
@@ -1,7 +1,7 @@
 package com.spiderpig86.snowflake.time;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
-
 import java.time.Clock;
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +11,8 @@ import lombok.ToString;
 /** Default {@link Time} class where a tick is 1 millisecond. */
 @ToString
 public class DefaultTime extends Time {
+
+  @VisibleForTesting static final long DEFAULT_EPOCH = 1577779200000L;
 
   private final Instant epoch;
   private final long tickDurationMs;
@@ -35,5 +37,9 @@ public class DefaultTime extends Time {
   @Override
   public long getTickDurationMs() {
     return tickDurationMs;
+  }
+
+  public static Time getDefault(@Nonnull final Clock clock) {
+    return new DefaultTime(clock, Instant.ofEpochMilli(DEFAULT_EPOCH));
   }
 }
--- src/test/java/com/spiderpig86/snowflake/time/DefaultTimeTest.java
+++ src/test/java/com/spiderpig86/snowflake/time/DefaultTimeTest.java
@@ -1,36 +1,49 @@
 package com.spiderpig86.snowflake.time;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import static com.spiderpig86.snowflake.time.DefaultTime.DEFAULT_EPOCH;
+import static org.mockito.Mockito.when;
 
 import java.time.Clock;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-
-import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class DefaultTimeTest {
-    private static final Instant NOW = Instant.now();
-    private static final Instant BEFORE = NOW.minus(5, ChronoUnit.MINUTES);
+  private static final Instant NOW = Instant.now();
+  private static final Instant BEFORE = NOW.minus(5, ChronoUnit.MINUTES);
 
-    @Mock private Clock clock;
+  @Mock private Clock clock;
 
-    @Test
-    public void testDefaultTime() {
-        // Arrange
-        when(clock.millis()).thenReturn(NOW.toEpochMilli());
+  @Test
+  public void testDefaultTime() {
+    // Arrange
+    when(clock.millis()).thenReturn(NOW.toEpochMilli());
 
-        // Act
-        final DefaultTime defaultTime = new DefaultTime(clock, BEFORE);
+    // Act
+    final Time defaultTime = new DefaultTime(clock, BEFORE);
 
-        // Assert
-        Assertions.assertEquals(BEFORE, defaultTime.getEpoch());
-        Assertions.assertEquals(300000L, defaultTime.getTick());
-        Assertions.assertEquals(1000L, defaultTime.getTickDurationMs());
-    }
+    // Assert
+    Assertions.assertEquals(BEFORE, defaultTime.getEpoch());
+    Assertions.assertEquals(300000L, defaultTime.getTick());
+    Assertions.assertEquals(1000L, defaultTime.getTickDurationMs());
+  }
+
+  @Test
+  public void testDefaultInstance() {
+    // Arrange
+    when(clock.millis()).thenReturn(NOW.toEpochMilli());
+
+    // Act
+    final Time defaultTime = DefaultTime.getDefault(clock);
+
+    // Assert
+    Assertions.assertEquals(Instant.ofEpochMilli(DEFAULT_EPOCH), defaultTime.getEpoch());
+    Assertions.assertEquals(NOW.minusMillis(DEFAULT_EPOCH).toEpochMilli(), defaultTime.getTick());
+    Assertions.assertEquals(1000L, defaultTime.getTickDurationMs());
+  }
 }
